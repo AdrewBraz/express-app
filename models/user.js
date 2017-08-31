8a91b3fe46c0b1f5b9475e285253d892df9c86ec
AndrewBraz <andbrazhnikov@gmail.com>
2017-08-31 16:16:44 +0300

    Changed config

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('../libs/mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -30,37 +30,37 @@ const UserSchema = new mongoose.Schema({
 
 //authentication
 
-UserSchema.statics.authenticate = (email, password, callback) => {
-  User.findOne({email: email})
-      .exec((error, user) => {
-        if(error){
-          return callback(error);
-        } else if(!user){
-          let err = new Error("User not found!");
-          err.status = 401;
-          return callback(err);
-        }
-        bcrypt.compare(password, user.password, (error, result) => {
-          if(result === true){
-            return callback(null, user);
-          } else{
-            return callback();
-          }
-        })
-      })   
-}
+// UserSchema.statics.authenticate = (email, password, callback) => {
+//   User.findOne({email: email})
+//       .exec((error, user) => {
+//         if(error){
+//           return callback(error);
+//         } else if(!user){
+//           let err = new Error("User not found!");
+//           err.status = 401;
+//           return callback(err);
+//         }
+//         bcrypt.compare(password, user.password, (error, result) => {
+//           if(result === true){
+//             return callback(null, user);
+//           } else{
+//             return callback();
+//           }
+//         })
+//       })   
+// }
 
 
-UserSchema.pre('save', (next) => {
-  let user = this;
-  bcrypt.hash(user.password, 10, (err, hash) =>{
-    if(err){
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
-})
+// UserSchema.pre('save', (next) => {
+//   let user = this;
+//   bcrypt.hash(user.password, 10, (err, hash) =>{
+//     if(err){
+//       return next(err);
+//     }
+//     user.password = hash;
+//     next();
+//   })
+// })
 
 const User = mongoose.model('User', UserSchema);
 
